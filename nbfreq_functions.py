2a922307565b5704d37d2dadccd17556c7dc938b
Complex and Sustainable Urban Networks (CSUN) Lab <csunlab@uic.edu>
2016-01-11 10:48:36 -0600

    Update nbfreq_functions.py

--- nbfreq_functions.py
+++ nbfreq_functions.py
@@ -1,6 +1,6 @@
 '''
-Citation: Derrible, S., and Ahmad, N., "Network-Based and Binless Frequency Analyses", under review
-(citation will be updated once the work will be published)
+Derrible S, Ahmad N (2015) Network-Based and Binless Frequency Analyses. PLoS ONE 10(11): e0142108. doi:10.1371/journal.pone.0142108
+Available at: http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0142108
 '''
 
 #Libraries needed to run the tool
